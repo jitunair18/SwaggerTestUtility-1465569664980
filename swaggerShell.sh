1edad28ab0e1163b0b01c013a888e38bfd61ae8a
jitunair18 <jitunair18@gmail.com>
2016-06-13 15:17:32 -0400

    build image too long

--- swaggerShell.sh
+++ swaggerShell.sh
@@ -2,5 +2,5 @@
 
 CLASSPATH=/tmp/output/SwaggerAsset.jar 
 java -cp $CLASSPATH com.ibmwatson.devopsservices.swaggertestasset.SwaggerCommandLine $1
-echo "check deploy container piece working or not"
+echo "check deploy container piece working taking too long"
 echo $?
