a0097fc26beb2cf842fe4bf235c1714043a8d989
jitunair18 <jitunair18@gmail.com>
2016-06-14 18:09:32 -0400

    return status code from shell script

--- swaggerShell.sh
+++ swaggerShell.sh
@@ -2,5 +2,3 @@
 
 CLASSPATH=/tmp/output/SwaggerAsset.jar 
 java -cp $CLASSPATH com.ibmwatson.devopsservices.swaggertestasset.SwaggerCommandLine $1
-echo "anything wrong with ibm containers 3:03 pm  change"
-echo $?
